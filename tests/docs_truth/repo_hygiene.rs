cfd4c25d69587d80cafd508f50f0fdfb9e0e36af
Lantos <39604275+lantos1618@users.noreply.github.com>
2026-05-17 22:16:00 +0100

    Remove return from stdlib linkedlist
    Remove return from stdlib linkedlist

--- tests/docs_truth/repo_hygiene.rs
+++ tests/docs_truth/repo_hygiene.rs
@@ -125,6 +125,7 @@ fn promoted_stdlib_modules_do_not_use_removed_return_keyword() {
         "stdlib/build.zen",
         "stdlib/collections/char.zen",
         "stdlib/collections/hashmap.zen",
+        "stdlib/collections/linkedlist.zen",
         "stdlib/collections/queue.zen",
         "stdlib/collections/set.zen",
         "stdlib/collections/vec.zen",
