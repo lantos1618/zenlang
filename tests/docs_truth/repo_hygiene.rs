33166007a8f5cd49ba7a93877f461232e67d56c0
Lantos <39604275+lantos1618@users.noreply.github.com>
2026-05-17 20:39:30 +0100

    Remove return from stdlib io facade
    Remove return from stdlib io facade

--- tests/docs_truth/repo_hygiene.rs
+++ tests/docs_truth/repo_hygiene.rs
@@ -128,6 +128,7 @@ fn promoted_stdlib_modules_do_not_use_removed_return_keyword() {
         "stdlib/fs.zen",
         "stdlib/io/eventfd.zen",
         "stdlib/io/inotify.zen",
+        "stdlib/io/io.zen",
         "stdlib/io/mux/epoll.zen",
         "stdlib/io/mux/poll.zen",
         "stdlib/io/net/pipe.zen",
