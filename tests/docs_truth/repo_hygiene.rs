e1d576cc25add71b89adc62cb86ef5623696d179
Lantos <39604275+lantos1618@users.noreply.github.com>
2026-05-17 22:32:11 +0100

    Remove return from stdlib file dir
    Remove return from stdlib file dir

--- tests/docs_truth/repo_hygiene.rs
+++ tests/docs_truth/repo_hygiene.rs
@@ -153,6 +153,7 @@ fn promoted_stdlib_modules_do_not_use_removed_return_keyword() {
         "stdlib/fs.zen",
         "stdlib/io/eventfd.zen",
         "stdlib/io/files/copy.zen",
+        "stdlib/io/files/dir.zen",
         "stdlib/io/inotify.zen",
         "stdlib/io/io.zen",
         "stdlib/io/mux/epoll.zen",
