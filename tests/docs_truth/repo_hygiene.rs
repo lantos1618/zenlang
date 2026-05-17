457d982f0c4711ebbf8c954595938beda4212b79
lantos1618 <l.leong1618@gmail.com>
2026-05-17 19:31:32 +0000

    Remove return from stdlib poll facade

--- tests/docs_truth/repo_hygiene.rs
+++ tests/docs_truth/repo_hygiene.rs
@@ -128,6 +128,7 @@ fn promoted_stdlib_modules_do_not_use_removed_return_keyword() {
         "stdlib/fs.zen",
         "stdlib/io/eventfd.zen",
         "stdlib/io/inotify.zen",
+        "stdlib/io/mux/poll.zen",
         "stdlib/io/net/pipe.zen",
         "stdlib/io/signal.zen",
         "stdlib/io/timerfd.zen",
