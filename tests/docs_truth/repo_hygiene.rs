79466e79394199cea4dd8da1ae19a7ef24c86aa3
lantos1618 <l.leong1618@gmail.com>
2026-05-17 21:54:39 +0000

    Remove return from stdlib process prctl

--- tests/docs_truth/repo_hygiene.rs
+++ tests/docs_truth/repo_hygiene.rs
@@ -173,6 +173,7 @@ fn promoted_stdlib_modules_do_not_use_removed_return_keyword() {
         "stdlib/sys/memfd.zen",
         "stdlib/sys/uname.zen",
         "stdlib/sys/process/process.zen",
+        "stdlib/sys/process/prctl.zen",
         "stdlib/sys/process/sched.zen",
         "stdlib/sys/random/getrandom.zen",
         "stdlib/sys/random/prng.zen",
