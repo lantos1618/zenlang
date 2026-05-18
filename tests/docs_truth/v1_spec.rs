a824a42f47aa68aa550b76d980912f7366bcd6da
lantos1618 <l.leong1618@gmail.com>
2026-05-18 20:58:36 +0000

    Pin function type parameter generic annotation diagnostic

--- tests/docs_truth/v1_spec.rs
+++ tests/docs_truth/v1_spec.rs
@@ -142,6 +142,7 @@ fn v1_spec_records_phase_one_feature_gates_and_test_backlog() {
         "emit_json_diagnostics_cast_target_annotation_missing_args_schema_matches_golden",
         "emit_json_diagnostics_nested_generic_annotation_inner_arity_schema_matches_golden",
         "emit_json_diagnostics_nested_generic_instantiation_inner_arity_schema_matches_golden",
+        "emit_json_diagnostics_function_type_parameter_annotation_arity_schema_matches_golden",
         "emit_json_diagnostics_generic_struct_constructor_arity_schema_matches_golden",
         "emit_json_diagnostics_generic_struct_constructor_missing_args_schema_matches_golden",
         "emit_json_diagnostics_generic_enum_constructor_arity_schema_matches_golden",
