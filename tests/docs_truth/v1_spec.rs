aa867362a62360c5563f442622c4500470527ab5
lantos1618 <l.leong1618@gmail.com>
2026-05-19 00:08:32 +0000

    Pin nongeneric builtin diagnostic

--- tests/docs_truth/v1_spec.rs
+++ tests/docs_truth/v1_spec.rs
@@ -156,6 +156,7 @@ fn v1_spec_records_phase_one_feature_gates_and_test_backlog() {
         "emit_json_diagnostics_generic_function_arity_schema_matches_golden",
         "emit_json_diagnostics_nongeneric_function_type_args_schema_matches_golden",
         "emit_json_diagnostics_nongeneric_module_function_type_args_schema_matches_golden",
+        "emit_json_diagnostics_nongeneric_builtin_function_type_args_schema_matches_golden",
         "emit_json_diagnostics_generic_function_type_arg_annotation_arity_schema_matches_golden",
         "emit_json_diagnostics_generic_method_type_arg_annotation_arity_schema_matches_golden",
         "emit_json_diagnostics_nongeneric_method_type_args_schema_matches_golden",
