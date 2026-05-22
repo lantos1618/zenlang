6a704b4eb0a67982874066f43e4a1d90b24caad8
lantos1618 <l.leong1618@gmail.com>
2026-05-22 20:34:22 +0000

    cover phantom generic behavior bounds

--- src/typechecker/tests/generic_behaviors/generic_bounds/type_args.rs
+++ src/typechecker/tests/generic_behaviors/generic_bounds/type_args.rs
@@ -32,6 +32,38 @@ main = () i32 {
 }
 
 #[test]
+fn generic_behavior_bound_accepts_phantom_generic_target_impl() {
+    let program = parse_program(
+        r#"
+Tagged<T>: behavior {
+    tag: (Self) StaticString
+}
+
+Marker<T>: { id: i32 }
+
+Marker<T>.implements(Tagged<T>) {
+    tag = (self: Marker<T>) StaticString { "marker" }
+}
+
+accept<T: Tagged<i32>> = (value: T) T {
+    value.tag()
+    value
+}
+
+main = () i32 {
+    marker = Marker<i32> { id: 7 }
+    accepted = accept(marker)
+    accepted.id
+}
+"#,
+    );
+
+    TypeChecker::new()
+        .check_program(&program)
+        .expect("phantom generic target impl should satisfy matching generic behavior bound");
+}
+
+#[test]
 fn generic_behavior_bound_with_type_args_rejects_mismatched_impl() {
     let program = parse_program(
         r#"
