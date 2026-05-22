666c0200c8480d04e73eb6127143d6b3e16997ab
lantos1618 <l.leong1618@gmail.com>
2026-05-22 14:51:06 +0000

    split typechecker AST declaration tasks

---
+++ src/typechecker/declaration_tasks_ast.rs
@@ -0,0 +1,130 @@
+enum AstTypeDeclarationTask<'a> {
+    Struct {
+        name: &'a str,
+        type_params: &'a [ast::TypeParam],
+        fields: &'a [StructField],
+    },
+    Enum {
+        name: &'a str,
+        type_params: &'a [ast::TypeParam],
+        variants: &'a [EnumVariant],
+    },
+}
+
+struct BehaviorDeclarationTask<'a> {
+    name: &'a str,
+    type_params: &'a [ast::TypeParam],
+    methods: &'a [BehaviorMethod],
+}
+
+struct AstImportDeclarationTask<'a> {
+    names: &'a [String],
+    module_path: &'a [String],
+}
+
+#[derive(Default)]
+struct AstDeclarationCollectionTasks<'a> {
+    behaviors: Vec<BehaviorDeclarationTask<'a>>,
+    types: Vec<AstTypeDeclarationTask<'a>>,
+    callable: Vec<CallableDeclarationTask<'a>>,
+    impl_blocks: Vec<ImplBlockDeclarationTask<'a>>,
+    imports: Vec<AstImportDeclarationTask<'a>>,
+    precollection_validations: AstPrecollectionValidationTasks<'a>,
+}
+
+struct AstStructFieldDefaultValidationTask<'a> {
+    type_params: &'a [ast::TypeParam],
+    fields: &'a [StructField],
+}
+
+enum AstTypeReferenceValidationTask<'a> {
+    Struct {
+        type_params: &'a [ast::TypeParam],
+        fields: &'a [StructField],
+    },
+    Enum {
+        type_params: &'a [ast::TypeParam],
+        variants: &'a [EnumVariant],
+    },
+    Function {
+        type_params: &'a [ast::TypeParam],
+        params: &'a [Param],
+        return_type: &'a Option<AstType>,
+        body: &'a Expression,
+    },
+    Method {
+        type_params: &'a [ast::TypeParam],
+        params: &'a [Param],
+        return_type: &'a Option<AstType>,
+        body: &'a Expression,
+    },
+    Behavior {
+        type_params: &'a [ast::TypeParam],
+        methods: &'a [BehaviorMethod],
+    },
+    ImplBlock {
+        methods: &'a [Declaration],
+    },
+    TopLevelExpr {
+        expr: &'a Expression,
+    },
+}
+
+enum SelfTypeContextValidationTask<'a> {
+    Struct {
+        fields: &'a [StructField],
+    },
+    Enum {
+        variants: &'a [EnumVariant],
+    },
+    Function {
+        params: &'a [Param],
+        return_type: &'a Option<AstType>,
+        body: &'a Expression,
+        span: Span,
+    },
+    Method {
+        params: &'a [Param],
+        return_type: &'a Option<AstType>,
+        body: &'a Expression,
+        span: Span,
+    },
+    Behavior {
+        methods: &'a [BehaviorMethod],
+    },
+    ImplBlock {
+        behavior_type_args: &'a [AstType],
+        methods: &'a [Declaration],
+        span: Span,
+    },
+    Requires {
+        behavior_type_args: &'a [AstType],
+        span: Span,
+    },
+    BehaviorExtends {
+        parent_type_args: &'a [AstType],
+        span: Span,
+    },
+    TopLevelExpr {
+        expr: &'a Expression,
+    },
+}
+
+#[derive(Default)]
+struct AstDeclarationValidationTasks<'a> {
+    behavior_associations: BehaviorAssociationValidationTasks<'a>,
+    type_references: Vec<AstTypeReferenceValidationTask<'a>>,
+    struct_field_defaults: Vec<AstStructFieldDefaultValidationTask<'a>>,
+}
+
+#[derive(Default)]
+struct AstPrecollectionValidationTasks<'a> {
+    self_type_contexts: Vec<SelfTypeContextValidationTask<'a>>,
+    behavior_associations: BehaviorAssociationValidationTasks<'a>,
+}
+
+impl<'a> BehaviorAssociationValidationTaskSource<'a> for AstDeclarationValidationTasks<'a> {
+    fn behavior_association_tasks(&self) -> &BehaviorAssociationValidationTasks<'a> {
+        &self.behavior_associations
+    }
+}
