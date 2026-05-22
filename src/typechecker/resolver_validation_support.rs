d41f017ed7484eceb112f1ca2bda5c7ebc27c5cf
lantos1618 <l.leong1618@gmail.com>
2026-05-22 10:14:52 +0000

    Split expected behavior edge support

--- src/typechecker/resolver_validation_support.rs
+++ src/typechecker/resolver_validation_support.rs
@@ -14,6 +14,7 @@ include!("resolver_validation_support/type_info_constructors.rs");
 include!("resolver_validation_support/type_helpers.rs");
 include!("resolver_validation_support/behavior_type_helpers.rs");
 include!("resolver_validation_support/behavior_refs.rs");
+include!("resolver_validation_support/expected_behavior_edges.rs");
 include!("resolver_validation_support/expected_locals.rs");
 include!("resolver_validation_support/expected_local_traversal.rs");
 include!("resolver_validation_support/expected_formatting.rs");
